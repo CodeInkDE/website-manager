24431cad357b286df597f18aacae5b6207dc7c17
Aiko Appeldorn <aiko@appeldorn.me>
2018-03-30 15:58:01 +0200

    move to subdirectories

--- start.sh
+++ start.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-VER='1.1.0'
+VER='1.2.0'
 _tmp="/tmp/answer.$$"
 TITLE="CodeInk - Manager"
+
 ##
 #
 # Functions
 #
 ##
-
 function greenMessage {
     echo -e "\\033[32;1m${@}\033[0m"
 }
@@ -35,7 +35,6 @@ function errorExit {
 }
 
 function installdialog {
-
 checkdialog=$(command -v dialog)
 
 if [[ $checkdialog = "" ]]; then
@@ -51,7 +50,6 @@ fi
 }
 
 function checkroot {
-
 if [ "`id -u`" != "0" ]; then
     redMessage "Wechsle zu dem Root Benutzer!"
     su root
@@ -60,56 +58,126 @@ if [ "`id -u`" != "0" ]; then
     errorExit "Nicht als Rootbenutzer ausgeführt, Abgebrochen!"
     exit
 	fi
-
 }
 
-function manageWebsites {
-domains=""
-list="$(ls -G /var/www/vhost)"
-leer="-->"
+function main_menu {
+    dialog --backtitle "$TITLE" --title " Main Menu - v$VER"\
+        --cancel-label "Quit" \
+        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
+        manageTLD "Manage TLDs"\
+        quit "Exit Manager" 2>$_tmp
 
-for d in $list
-do
-  domains="$domains $d $leer "
-done
-
-domains="$domains add $leer"
-
-dialog --backtitle "$TITLE" --title " Manage Websites " --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
-#echo "$website"
-website=`cat $_tmp`
-if [[ $website != "add" && $website != "Back" ]]; then
-    manageWebsite
-else if [[ $website == "add" ]]; then
-    addWebsite
-else
-    manageWebsites
-fi
-fi
+    opt=${?}
+    if [ $opt != 0 ]; then rm $_tmp; exit; fi;
+    menuitem=`cat $_tmp`
+    case $menuitem in
+        manageTLD) tld_menu;;
+        quit) rm $_tmp; exit 0;;
+    esac
 }
 
-function addWebsite {
+### Menus ###
+function tld_menu {
+    domains=""
+    list="$(ls -G /var/www/vhost)"
+    leer="-->"
+
+    for d in $list
+    do
+        domains="$domains $d $leer "
+    done
+
+    domains="$domains add $leer"
+
+    dialog --backtitle "$TITLE" --title " Manage TLD-Domains " --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
+    website=`cat $_tmp`
+    if [[ $website != "add" && $website != "Back" ]]; then
+        manageTld
+    else if [[ $website == "add" ]]; then
+        addTld
+    else
+        main_menu
+    fi
+    fi
+}
+
+function subdomain_menu {
+    tld = $website
+    domains=""
+    list="$(ls -G /var/www/vhost/$tld/)"
+    leer="-->"
+
+    for d in $list
+    do
+        domains="$domains $d $leer "
+    done
+
+    domains="$domains add $leer"
+
+    dialog --backtitle "$TITLE" --title " Manage Subdomains of $tld" --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
+    subdomain=`cat $_tmp`
+    if [[ $subdomain != "add" && $subdomain != "Back" ]]; then
+        manageSubdomain "$tld"
+    else if [[ $subdomain == "add" ]]; then
+        addSubdomain "$subdomain"
+    else
+        main_menu
+    fi
+    fi
+}
+
+### TLD ###
+function manageTld {
+    formatted=$(echo "$website" | sed -r 's/\.//g')
+
+    if [ -f "/etc/php/7.0/fpm/pool.d/$formatted.conf" ]; then
+        php="[ Activated ]"
+    else
+        php="[ Deactivated ]"
+    fi
+
+    dialog --backtitle "$TITLE" --title " Manage Website - $website"\
+        --cancel-label "Back" \
+        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
+        subdomains "Subdomains"\
+        php "$php"\
+        changePW "Reset password" \
+        delete "Delete website"\
+        back "Back" 2>$_tmp
+
+    menuitem=`cat $_tmp`
+    case $menuitem in
+        subdomains) subdomain_menu "$website";;
+        php) manageTld;;
+        changePW) changePW "www-$formatted";;
+        delete) deleteTld "$website";;
+        quit) rm $_tmp; exit 0;;
+    esac
+}
+
+function addTld {
 	domain=$( \
-		dialog  --title "Add Websites" \
+		dialog  --title "Add TLD" \
 				--cancel-label "Cancel" \
-			    --inputbox "Type in your Domain (example: hosted4u.de)" 8 40 \
+			    --inputbox "Type in your TLD-Domain (example: domain.de)" 8 40 \
 		3>&1 1>&2 2>&3 3>&- \
 	)
 
     if [ -z "$domain" ]; then
-        addWebsite
+        addTld
         exit 0
     fi
 
 	if [ -d "/var/www/vhost/$domain/" ]; then
-        errorExit "Domain already exits!"
+        errorExit "TLD already exits!"
     fi
 
-    mkdir "/var/www/vhost/$domain/"
+    mkdir -p "/var/www/vhost/$domain/httpdocs/"
     formatted=$(echo "$domain" | sed -r 's/\.//g')
 
     cp configs/pool.default /etc/php/7.0/fpm/pool.d/"$formatted".conf
     sed -i "s/%DOMAIN%/$formatted/g" /etc/php/7.0/fpm/pool.d/"$formatted".conf
+    sed -i "s/%USER%/$formatted/g" /etc/php/7.0/fpm/pool.d/"$formatted".conf
 
     pw=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15 ; echo '')
     useradd www-"$formatted" --home-dir "/var/www/vhost/$domain/" --no-create-home --shell /bin/nologin --password "$pw" --groups www-data
@@ -117,17 +185,17 @@ function addWebsite {
     cp configs/nginx-sites.default /etc/nginx/sites-enabled/"$domain"
     sed -i "s/%DOMAIN%/$domain/g" /etc/nginx/sites-enabled/"$domain"
     sed -i "s/%FORMATTED%/$formatted/g" /etc/nginx/sites-enabled/"$domain"
+    sed -i "s/%DIRECTORY%/httpdocs/g" /etc/nginx/sites-enabled/"$domain"
 
     /usr/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d  "$domain"
 
     service php7.0-fpm reload
     service nginx reload
 
-    cp configs/index.html "/var/www/vhost/$domain/index.html"
-    sed -i "s/%DOMAIN%/$domain/g" "/var/www/vhost/$domain/index.html"
-    chown -R www-"$formatted":www-data "/var/www/vhost/$domain/"
-
-    clear
+    cp configs/index.html "/var/www/vhost/$domain/httpdocs/index.html"
+    sed -i "s/%DOMAIN%/$domain/g" "/var/www/vhost/$domain/httpdocs/index.html"
+    chown root:root "/var/www/vhost/$domain/"
+    chown -R www-"$formatted":www-data "/var/www/vhost/$domain/httpdocs/"
 
     echo "**************************"
     echo "Domain: $domain"
@@ -138,24 +206,23 @@ function addWebsite {
     exit 0
 }
 
-function deleteWebsite {
+function deleteTLD {
     domain=${@}
 
-    dialog --title "Delete Website" --yesno "Remove $domain ?" 8 40
+    dialog --title "Delete TLD" --yesno "Remove $domain and all Subdomains!?" 8 40
     response=$?
 
     if [ $response = 1 ]; then
-        manageWebsites
+        manageTLD
         exit 0
     fi
-
 
     if [ -z "$domain" ]; then
         errorExit "No parameter!"
     fi
 
 	if ! [ -d "/var/www/vhost/$domain/" ]; then
-        errorExit "Website doesn't exists!"
+        errorExit "TLD doesn't exists!"
     fi
 
     tar cfz backups/$domain.tar.gz "/var/www/vhost/$domain/"
@@ -172,8 +239,6 @@ function deleteWebsite {
     service nginx reload
     deluser www-"$formatted"
 
-    clear
-
     echo "**************************"
     echo "Domain: $domain"
     echo "Status: DELETED"
@@ -183,38 +248,123 @@ function deleteWebsite {
     exit 0
 }
 
-
-
-
-function manageWebsite {
-
-    formatted=$(echo "$website" | sed -r 's/\.//g')
+### Subdomain ###
+function manageSubdomain {
+    tld=${@}
 
     if [ -f "/etc/php/7.0/fpm/pool.d/$formatted.conf" ]; then
         php="[ Activated ]"
     else
-        php="[ Dectivated ]"
+        php="[ Deactivated ]"
     fi
 
-
-    dialog --backtitle "$TITLE" --title " Manage Website - $website"\
+    dialog --backtitle "$TITLE" --title " Manage Subdomain - $tld"\
         --cancel-label "Back" \
         --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
         php "$php"\
-        changePW "Reset password" \
-        delete "Delete website"\
+        delete "Delete subdomain"\
         back "Back" 2>$_tmp
 
     menuitem=`cat $_tmp`
-    #echo "menu=$menuitem"
     case $menuitem in
-        php) manageWebsites;;
-        changePW) changePW "www-$formatted";;
-        delete) deleteWebsite "$website";;
+        php) manageSubdomain "$tld";;
+        delete) deleteSubdomain "$tld";;
         quit) rm $_tmp; exit 0;;
     esac
-
 }
+
+function addSubdomain {
+    tld=${@}
+
+	subdomain=$( \
+		dialog  --title "Add TLD" \
+				--cancel-label "Cancel" \
+			    --inputbox "Type in your Subdomain (example: subdomain.$tld)" 8 40 \
+		3>&1 1>&2 2>&3 3>&- \
+	)
+
+    if [ -z "$subdomain" ]; then
+        addSubdomain
+        exit 0
+    fi
+
+	if [ -d "/var/www/vhost/$tld/$subdomain/" ]; then
+        errorExit "Subdomain already exits!"
+    fi
+
+    mkdir -p "/var/www/vhost/$tld/$subdomain/"
+    formattedSub=$(echo "$subdomain" | sed -r 's/\.//g')
+    formattedTld=$(echo "$tld" | sed -r 's/\.//g')
+
+    cp configs/pool.default /etc/php/7.0/fpm/pool.d/"$formatted".conf
+    sed -i "s/%DOMAIN%/$formattedSub/g" /etc/php/7.0/fpm/pool.d/"$formatted".conf
+    sed -i "s/%USER%/$formattedTld/g" /etc/php/7.0/fpm/pool.d/"$formatted".conf
+
+    cp configs/nginx-sites.default /etc/nginx/sites-enabled/"$subdomain"
+    sed -i "s/%DOMAIN%/$subdomain/g" /etc/nginx/sites-enabled/"$subdomain"
+    sed -i "s/%FORMATTED%/$formattedSub/g" /etc/nginx/sites-enabled/"$subdomain"
+    sed -i "s/%DIRECTORY%/$subdomain/g" /etc/nginx/sites-enabled/"$subdomain"
+
+    /usr/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d  "$subdomain"
+
+    service php7.0-fpm reload
+    service nginx reload
+
+    cp configs/index.html "/var/www/vhost/$tld/$subdomain/index.html"
+    sed -i "s/%DOMAIN%/$tld/g" "/var/www/vhost/$tld/$subdomain/index.html"
+    chown -R www-"$formatted":www-data "/var/www/vhost/$tld/$subdomain/"
+
+    echo "**************************"
+    echo "TLD: $tld"
+    echo "Subdomain: $subdomain"
+    echo "User: www-$formattedTld"
+    echo "**************************"
+
+    exit 0      
+}
+
+function deleteSubdomain {
+    tld=${@}
+
+    dialog --title "Delete TLD" --yesno "Remove $subdomain" 8 40
+    response=$?
+
+    if [ $response = 1 ]; then
+        manageTLD
+        exit 0
+    fi
+
+    if [ -z "$domain" ]; then
+        errorExit "No parameter!"
+    fi
+
+	if ! [ -d "/var/www/vhost/$domain/$subdomain" ]; then
+        errorExit "TLD doesn't exists!"
+    fi
+
+    tar cfz backups/$subdomain.tar.gz "/var/www/vhost/$tld/$subdomain/"
+    rm -R "/var/www/vhost/$tld/$subdomain/"
+
+    formatted=$(echo "$subdomain" | sed -r 's/\.//g')
+
+    rm /etc/php/7.0/fpm/pool.d/"$formatted".conf
+    rm /etc/nginx/sites-enabled/"$subdomain"
+    rm -rf "/etc/letsencrypt/live/$subdomain"
+    rm "/etc/letsencrypt/renewal/$subdomain.conf"
+
+    service php7.0-fpm reload
+    service nginx reload
+
+    echo "**************************"
+    echo "Domain: $subdomain"
+    echo "Status: DELETED"
+    echo "Backup: backups/$subdomain.tar.gz"
+    echo "**************************"
+
+    exit 0
+}
+
+
 
 function changePW {
     user=${@}
@@ -230,24 +380,6 @@ function changePW {
     echo "**************************"
 
     exit 0
-}
-
-
-function main_menu {
-    dialog --backtitle "$TITLE" --title " Main Menu - v$VER"\
-        --cancel-label "Quit" \
-        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
-        manageWebsites "Manage Websites"\
-        quit "Exit Manager" 2>$_tmp
-
-    opt=${?}
-    if [ $opt != 0 ]; then rm $_tmp; exit; fi;
-    menuitem=`cat $_tmp`
-    #echo "menu=$menuitem"
-    case $menuitem in
-        manageWebsites) manageWebsites;;
-        quit) rm $_tmp; exit 0;;
-    esac
 }
 
 ##
