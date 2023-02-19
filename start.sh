261542b8c7d3eece97c50082f46d275e58ed5aba
Aiko Appeldorn <aiko@appeldorn.dev>
2023-02-19 16:30:42 +0100

    #7 PHP config
    o made php configurable

--- start.sh
+++ start.sh
@@ -5,11 +5,7 @@ PHPVer='8.1'
 _tmp="/tmp/answer.$$"
 TITLE="Website Manager - Nevondo"
 
-##
-#
-# Functions
-#
-##
+### Functions ###
 function greenMessage {
     echo -e "\\033[32;1m${@}\033[0m"
 }
@@ -36,130 +32,165 @@ function errorExit {
 }
 
 function installDialog {
-checkdialog=$(command -v dialog)
+    checkdialog=$(command -v dialog)
 
-if [[ $checkdialog = "" ]]; then
-    greenMessage "Das Paket Dialog wird für dieses Skript benötigt und wird in 10 Sekunden installiert."
-    sleep 1
-    redMessage "Möchtest du das Paket nicht installieren, breche die Installation mit CTRL + C ab."
-    sleep 10
-    trap '' 2 # CTRL + C Block start
-    apt-get update # 2>&1 > /dev/null
-    apt-get install dialog -y # 2>&1 > /dev/null
-fi
+    if [[ $checkdialog = "" ]]; then
+        greenMessage "Das Paket Dialog wird für dieses Skript benötigt und wird in 10 Sekunden installiert."
+        sleep 1
+        redMessage "Möchtest du das Paket nicht installieren, breche die Installation mit CTRL + C ab."
+        sleep 10
+        trap '' 2
+        apt-get update
+        apt-get install dialog -y
+    fi
 
-# check for updates
-git pull
-
+    # check for updates
+    git pull
 }
 
 function checkRoot {
-if [ "`id -u`" != "0" ]; then
-    redMessage "Wechsle zu dem Root Benutzer!"
-    su root
+    if [ "$(id -u)" != "0" ]; then
+        redMessage "Wechsle zu dem Root Benutzer!"
+        su root
     fi
-if [ "`id -u`" != "0" ]; then
-    errorExit "Nicht als Rootbenutzer ausgeführt, Abgebrochen!"
-    exit
+    if [ "$(id -u)" != "0" ]; then
+        errorExit "Nicht als Rootbenutzer ausgeführt, Abgebrochen!"
+        exit
     fi
 }
 
-function execute_MainMenu {
-    dialog --backtitle "$TITLE" --title " Main Menu - v$VER"\
-        --cancel-label "Quit" \
-        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
-        manageTLD "Manage TLDs"\
-        quit "Exit Manager" 2>$_tmp
+function changePW {
+    user=${@}
+    pw=$(
+        head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15
+        echo ''
+    )
 
-    opt=${?}
-    if [ $opt != 0 ]; then rm $_tmp; exit; fi;
-    menuitem=`cat $_tmp`
-    case $menuitem in
-        manageTLD) tld_menu;;
-        quit) rm $_tmp; exit 0;;
-    esac
+    echo "$user:$pw" | chpasswd
+
+    clear
+    echo "**************************"
+    echo "User: $user"
+    echo "Password: $pw"
+    echo "**************************"
+
+    exit 0
+}
+
+function removeDependencies {
+    value=$1
+    formattedValue=$(echo "$value" | sed -r 's/\.//g')
+    rm /etc/php/"$PHPVer"/fpm/pool.d/"$formattedValue".conf
+    rm /etc/nginx/sites-enabled/"$value"
+    /usr/local/bin/certbot revoke --cert-path "/etc/letsencrypt/archive/$value/cert1.pem"
+    rm -rf "/etc/letsencrypt/live/$value"
+    rm -rf "/etc/letsencrypt/archive/$value"
+    rm "/etc/letsencrypt/renewal/$value.conf"
+}
+
+function addPhp {
+    domain=$1
+    user=$2
+    formatted=$(echo "$domain" | sed -r 's/\.//g')
+
+    cp configs/pool.default /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
+    sed -i "s/%DOMAIN%/$formatted/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
+    sed -i "s/%USER%/$user/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
+    sed -i "s/%PHPVERSION%/$PHPVer/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
+
+    sed '/### PHP ###/r configs/php.template' /etc/nginx/sites-enabled/"$domain"
+    sed -i "s/%PHPVERSION%/$PHPVer/g" /etc/nginx/sites-enabled/"$domain"
+    service php"$PHPVer"-fpm reload
+}
+
+function removePhp {
+    domain=$1
+    formatted=$(echo "$domain" | sed -r 's/\.//g')
+
+    rm /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
+    sed '/### PHP ###,/### PHP ###/d' /etc/nginx/sites-enabled/"$domain"
 }
 
 ### Menus ###
+function execute_MainMenu {
+    dialog --backtitle "$TITLE" --title " Main Menu - v$VER"\
+    --cancel-label "Quit" \
+    --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
+    manageTLD "Manage TLDs"\
+    quit "Exit Manager" 2>$_tmp
+
+    opt=${?}
+    if [ $opt != 0 ]; then
+        rm $_tmp
+        exit
+    fi
+    menuitem=$(cat $_tmp)
+    case $menuitem in
+    manageTLD) tld_menu ;;
+    quit)
+        rm $_tmp
+        exit 0
+        ;;
+    esac
+}
+
+### TLD ###
 function tld_menu {
     domains=""
     list="$(ls -G /var/www/vhost)"
     leer="-->"
 
-    for d in $list
-    do
+    for d in $list; do
         domains="$domains $d $leer "
     done
 
     domains="$domains add $leer"
 
     dialog --backtitle "$TITLE" --title " Manage TLD-Domains " --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
-    website=`cat $_tmp`
+    website=$(cat $_tmp)
     if [[ $website != "add" && $website != "Back" ]]; then
         manageTld
-    else if [[ $website == "add" ]]; then
-        addTld
     else
-        main_menu
-    fi
-    fi
-}
-
-function subdomain_menu {
-    tld=$1
-    domains=""
-    list="$(ls -G /var/www/vhost/$tld/)"
-    leer="-->"
-
-    for d in $list
-    do
-        if [[ $d != "httpdocs" ]] && [[ $d != "logs" ]]; then
-            domains="$domains $d $leer "
+        if [[ $website == "add" ]]; then
+            addTld
+        else
+            main_menu
         fi
-    done
-
-    domains="$domains add $leer"
-
-    dialog --backtitle "$TITLE" --title " Manage Subdomains of $tld" --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
-    subdomain=`cat $_tmp`
-    if [[ $subdomain != "add" && $subdomain != "Back" ]]; then
-        manageSubdomain "$tld" "$subdomain"
-    else if [[ $subdomain == "add" ]]; then
-        addSubdomain "$tld"
-    else
-        main_menu
-    fi
     fi
 }
 
-### TLD ###
 function manageTld {
     formatted=$(echo "$website" | sed -r 's/\.//g')
 
     dialog --backtitle "$TITLE" --title " Manage TLD - $website"\
-        --cancel-label "Back" \
-        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
-        subdomains "Subdomains"\
-        changePW "Reset password" \
-        delete "Delete"\
-        back "Back" 2>$_tmp
+    --cancel-label "Back" \
+    --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
+    subdomains "Subdomains"\
+    changePW "Reset password" \
+    delete "Delete"\
+    back "Back" 2>$_tmp
 
-    menuitem=`cat $_tmp`
+    menuitem=$(cat $_tmp)
     case $menuitem in
-        subdomains) subdomain_menu "$website";;
-        php) manageTld;;
-        changePW) changePW "www-$formatted";;
-        delete) deleteTld "$website";;
-        quit) rm $_tmp; exit 0;;
+    subdomains) subdomain_menu "$website" ;;
+    php) manageTld ;;
+    changePW) changePW "www-$formatted" ;;
+    delete) deleteTld "$website" ;;
+    quit)
+        rm $_tmp
+        exit 0
+        ;;
     esac
 }
 
 function addTld {
-    domain=$( \
-        dialog  --title "Add TLD" \
-                --cancel-label "Cancel" \
-                --inputbox "Type in your TLD-Domain (example: domain.de)" 8 40 \
-        3>&1 1>&2 2>&3 3>&- \
+    domain=$(
+        \
+        dialog --title "Add TLD" \
+        --cancel-label "Cancel" \
+        --inputbox "Type in your TLD-Domain (example: domain.de)" 8 40 \
+        \
+        3>&1 1>&2 2>&3 3>&-
     )
 
     if [ -z "$domain" ]; then
@@ -175,12 +206,10 @@ function addTld {
     mkdir -p "/var/www/vhost/$domain/logs/"
     formatted=$(echo "$domain" | sed -r 's/\.//g')
 
-    cp configs/pool.default /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
-    sed -i "s/%DOMAIN%/$formatted/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
-    sed -i "s/%USER%/$formatted/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
-    sed -i "s/%PHPVERSION%/$PHPVer/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formatted".conf
-
-    pw=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15 ; echo '')
+    pw=$(
+        head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15
+        echo ''
+    )
     useradd www-"$formatted" --home-dir "/var/www/vhost/$domain/" --no-create-home --shell /bin/nologin --password "$pw" --groups www-data
 
     cp configs/nginx-tld.default /etc/nginx/sites-enabled/"$domain"
@@ -189,9 +218,9 @@ function addTld {
     sed -i "s/%FORMATTED%/$formatted/g" /etc/nginx/sites-enabled/"$domain"
     sed -i "s/%DIRECTORY%/httpdocs/g" /etc/nginx/sites-enabled/"$domain"
 
-    /usr/local/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d  "$domain" -d "www.$domain"
+    addPhp $domain $formatted
 
-    service php"$PHPVer"-fpm reload
+    /usr/local/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d "$domain" -d "www.$domain"
     service nginx reload
 
     cp configs/index.html "/var/www/vhost/$domain/httpdocs/index.html"
@@ -231,8 +260,7 @@ function deleteTld {
     tar cfz backups/$domain.tar.gz "/var/www/vhost/$domain/"
 
     list="$(ls -G /var/www/vhost/$domain/)"
-    for subdomain in $list
-    do
+    for subdomain in $list; do
         if [[ $subdomain != "httpdocs" ]] && [[ $subdomain != "logs" ]]; then
             rm -R "/var/www/vhost/$domain/$subdomain/"
             removeDependencies $subdomain
@@ -258,32 +286,64 @@ function deleteTld {
 }
 
 ### Subdomain ###
+function subdomain_menu {
+    tld=$1
+    domains=""
+    list="$(ls -G /var/www/vhost/$tld/)"
+    leer="-->"
+
+    for d in $list; do
+        if [[ $d != "httpdocs" ]] && [[ $d != "logs" ]]; then
+            domains="$domains $d $leer "
+        fi
+    done
+
+    domains="$domains add $leer"
+
+    dialog --backtitle "$TITLE" --title " Manage Subdomains of $tld" --cancel-label "Back" --menu "Move using [UP] [Down], [Enter] to select" 17 60 10 $domains 2>$_tmp
+    subdomain=$(cat $_tmp)
+    if [[ $subdomain != "add" && $subdomain != "Back" ]]; then
+        manageSubdomain "$tld" "$subdomain"
+    else
+        if [[ $subdomain == "add" ]]; then
+            addSubdomain "$tld"
+        else
+            main_menu
+        fi
+    fi
+}
+
 function manageSubdomain {
     tld=$1
     subdomain=$2
 
     dialog --backtitle "$TITLE" --title " Manage Subdomain - $tld"\
-        --cancel-label "Back" \
-        --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
-        php "$php"\
-        delete "Delete subdomain"\
-        back "Back" 2>$_tmp
+    --cancel-label "Back" \
+    --menu "Move using [UP] [Down], [Enter] to select" 17 60 10\
+    php "$php"\
+    delete "Delete subdomain"\
+    back "Back" 2>$_tmp
 
-    menuitem=`cat $_tmp`
+    menuitem=$(cat $_tmp)
     case $menuitem in
-        delete) deleteSubdomain "$tld" "$subdomain";;
-        quit) rm $_tmp; exit 0;;
+    delete) deleteSubdomain "$tld" "$subdomain" ;;
+    quit)
+        rm $_tmp
+        exit 0
+        ;;
     esac
 }
 
 function addSubdomain {
     tld=$1
 
-    subdomain=$( \
-        dialog  --title "Add Subdomain" \
-                --cancel-label "Cancel" \
-                --inputbox "Type in your Subdomain (example: subdomain.$tld)" 8 40 \
-        3>&1 1>&2 2>&3 3>&- \
+    subdomain=$(
+        \
+        dialog --title "Add Subdomain" \
+        --cancel-label "Cancel" \
+        --inputbox "Type in your Subdomain (example: subdomain.$tld)" 8 40 \
+        \
+        3>&1 1>&2 2>&3 3>&-
     )
 
     if [ -z "$subdomain" ]; then
@@ -299,20 +359,15 @@ function addSubdomain {
     formattedSub=$(echo "$subdomain" | sed -r 's/\.//g')
     formattedTld=$(echo "$tld" | sed -r 's/\.//g')
 
-    cp configs/pool.default /etc/php/"$PHPVer"/fpm/pool.d/"$formattedSub".conf
-    sed -i "s/%DOMAIN%/$formattedSub/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formattedSub".conf
-    sed -i "s/%USER%/$formattedTld/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formattedSub".conf
-    sed -i "s/%PHPVERSION%/$PHPVer/g" /etc/php/"$PHPVer"/fpm/pool.d/"$formattedSub".conf
-
     cp configs/nginx-subdomain.default /etc/nginx/sites-enabled/"$subdomain"
     sed -i "s/%TLD%/$tld/g" /etc/nginx/sites-enabled/"$subdomain"
     sed -i "s/%DOMAIN%/$subdomain/g" /etc/nginx/sites-enabled/"$subdomain"
     sed -i "s/%FORMATTED%/$formattedSub/g" /etc/nginx/sites-enabled/"$subdomain"
     sed -i "s/%DIRECTORY%/$subdomain/g" /etc/nginx/sites-enabled/"$subdomain"
 
-    /usr/local/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d  "$subdomain"
+    addPhp $subdomain $formattedTld
 
-    service php"$PHPVer"-fpm reload
+    /usr/local/bin/certbot certonly --webroot -w /var/www/letsencrypt/ -d "$subdomain"
     service nginx reload
 
     cp configs/index.html "/var/www/vhost/$tld/$subdomain/index.html"
@@ -364,37 +419,7 @@ function deleteSubdomain {
     exit 0
 }
 
-function changePW {
-    user=${@}
-    pw=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 15 ; echo '')
-
-    echo "$user:$pw"|chpasswd
-
-    clear
-    echo "**************************"
-    echo "User: $user"
-    echo "Password: $pw"
-    echo "**************************"
-
-    exit 0
-}
-
-function removeDependencies {
-    value=$1
-    formattedValue=$(echo "$value" | sed -r 's/\.//g')
-    rm /etc/php/"$PHPVer"/fpm/pool.d/"$formattedValue".conf
-    rm /etc/nginx/sites-enabled/"$value"
-    /usr/local/bin/certbot revoke --cert-path "/etc/letsencrypt/archive/$value/cert1.pem"
-    rm -rf "/etc/letsencrypt/live/$value"
-    rm -rf "/etc/letsencrypt/archive/$value"
-    rm "/etc/letsencrypt/renewal/$value.conf"
-}
-
-##
-#
-# Main
-#
-##
+### Main ###
 checkRoot
 installDialog
 
